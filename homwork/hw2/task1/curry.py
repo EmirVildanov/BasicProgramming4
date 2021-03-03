a676725705d89fb91483891cb77b2a3cd528abbe
EmirVildanov <reddog201030@gmail.com>
2021-03-04 01:12:54 +0300

    Added typing, docstrings and few tests

--- homwork/hw2/task1/curry.py
+++ homwork/hw2/task1/curry.py
@@ -1,9 +1,19 @@
+from collections import Callable
 from inspect import signature
+from typing import List
 
 
-def check_arity(fun, arity):
+def check_arity(function: Callable, arity: int):
+    """
+    :param function: function for checking arity
+    :param arity: int value of wanted function arity
+    :raises valueError: if arity is negative or incompatible with passed function
+    :return: None
+    """
+    if arity < 0:
+        raise ValueError("Arity can not be negative")
     has_args = False
-    fun_parameters = signature(fun).parameters
+    fun_parameters = signature(function).parameters
     fun_parameters_number = len(fun_parameters)
     if "args" in fun_parameters:
         has_args = True
@@ -13,32 +23,46 @@ def check_arity(fun, arity):
         elif fun_parameters_number > arity:
             raise ValueError("Arity is smaller than fun arguments number")
     elif arity < fun_parameters_number - 1:
-        raise ValueError("Arity is smaller than named functions arguments number")
+        raise ValueError("Arity is smaller than positional functions arguments number")
 
 
-def curry_explicit(fun, arity, passed_args=None):
-    if arity < 0:
-        raise ValueError("Arity can not be negative")
-    check_arity(fun, arity)
+def curry_explicit(function: Callable, arity: int, passed_args: List = None):
+    """
+    :param function: function that should be curried
+    :param arity: int value of wanted function arity
+    :param passed_args: arguments that are wanted to be splitted
+    :raises valueError: if arity is negative or incompatible with passed function
+    :return: None
+    """
+    check_arity(function, arity)
     if passed_args is None:
         passed_args = []
 
-    def wrapper(*args):
+    def curried_function(*args):
         if arity != 0 and len(args) != 1:
             raise ValueError("Function with non null arity must have only one argument")
         cur_args = [*passed_args, *args]
         if len(cur_args) < arity:
-            return curry_explicit(fun, arity, cur_args)
+            return curry_explicit(function, arity, cur_args)
         elif len(cur_args) == arity:
-            return fun(*cur_args)
+            return function(*cur_args)
         else:
             raise ValueError("Too many arguments")
 
-    return wrapper
+    return curried_function
 
 
-def uncurry_explicit(function, arity):
-    def returning_lambda(*args):
+def uncurry_explicit(function: Callable, arity: int):
+    """
+    :param function: function that should be uncurried
+    :param arity: int value of wanted function arity
+    :raises valueError: if arity is incompatible with passed function
+    :return: None
+    """
+    if arity < 0:
+        raise ValueError("Arity can not be negative")
+
+    def uncurried_function(*args):
         if len(args) < arity:
             raise ValueError("Arity is bigger than passed arguments number")
         elif len(args) > arity:
@@ -50,7 +74,7 @@ def uncurry_explicit(function, arity):
             del args[0]
         return result
 
-    return returning_lambda
+    return uncurried_function
 
 
 if __name__ == "__main__":
--- tests/hw2/task1/test_curry.py
+++ tests/hw2/task1/test_curry.py
@@ -5,6 +5,37 @@ from homwork.hw2.task1.curry import curry_explicit, uncurry_explicit
 
 
 class CurryTestCase(unittest.TestCase):
+    def test_should_raise_error_when_negative_arity_is_passed_to_curry(self):
+        with self.assertRaises(ValueError) as context:
+            curry_explicit(lambda x: print(x), -1)
+
+        self.assertTrue("Arity can not be negative" in str(context.exception))
+
+    def test_should_raise_error_when_negative_arity_is_passed_to_uncurry(self):
+        with self.assertRaises(ValueError) as context:
+            uncurry_explicit(lambda x: print(x), -1)
+
+        self.assertTrue("Arity can not be negative" in str(context.exception))
+
+    def test_should_raise_error_when_function_has_more_arguments_than_passed_arity(self):
+        with self.assertRaises(ValueError) as context:
+            curry_explicit(lambda x, y: x+y, 1)
+
+        self.assertTrue("Arity is smaller than fun arguments number" in str(context.exception))
+
+    def test_should_raise_error_when_function_has_less_arguments_than_passed_arity(self):
+        with self.assertRaises(ValueError) as context:
+            curry_explicit(lambda x, y: x+y, 3)
+
+        self.assertTrue("Arity is bigger than fun arguments number" in str(context.exception))
+
+    def test_should_raise_error_when_function_positional_arguments_number_is_bigger_than_arity(self):
+        with self.assertRaises(ValueError) as context:
+            curry_explicit(lambda x, y, *args: print(f"{x} {y} {args}"), 1)
+
+        print(context.exception)
+        self.assertTrue("Arity is smaller than positional functions arguments number" in str(context.exception))
+
     def test_should_curry_simple_function(self):
         def f(arg1: int):
             return arg1
@@ -25,3 +56,11 @@ class CurryTestCase(unittest.TestCase):
 
         curried_f = curry_explicit(f, 0)
         self.assertEqual(1, curried_f())
+
+    def should_uncurry_function_with_0_arguments(self):
+        def f():
+            return 1
+
+        curried_f = curry_explicit(f, 0)
+        uncurried_f = uncurry_explicit(curried_f, 0)
+        self.assertEqual(1, uncurried_f())
