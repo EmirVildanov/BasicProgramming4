eea1094676ed798df3ba270096ea2d87621333f4
EmirVildanov <reddog201030@gmail.com>
2021-03-04 20:44:22 +0300

    Added docstrings, positional arguments support typing and tests

--- homework/hw2/task3/smart_args.py
+++ homework/hw2/task3/smart_args.py
@@ -1,92 +1,65 @@
 import functools
+from collections.abc import Callable
 from inspect import signature, getfullargspec
 from copy import deepcopy
-
-
-# Типа если агрумент по умолчанию Isolated, то его надо скопировать, а не использовать переданный
-# Если Evaluated, то его надо зафиксировать
 import random
 
 
 class Isolated:
     def __init__(self, arg=None):
+        """
+        :param arg: argument passing for smart_args
+        :raises valueError in case Evaluated is passed inside
+        """
+        if isinstance(arg, Evaluated):
+            raise ValueError("Evaluated argument is not supported by Isolated")
         self.arg = arg
 
 
 class Evaluated:
     def __init__(self, func):
+        """
+        :param func: func passing for smart_args
+        :raises valueError in case Isolated is passed inside
+        """
+        if isinstance(func, Isolated):
+            raise ValueError("Isolated argument is not supported by Evaluated")
         if len(signature(func).parameters) != 0:
-            raise ValueError("Evaluated eat only functions with no arguments")
+            raise ValueError("Only functions with no arguments are supported by Evaluated")
         self.func = func
 
 
-def smart_args(func=None):
+def smart_args(func=None, *, positional_args_on: bool = False) -> Callable:
+    """
+    :param func: inner function for decorator
+    :param positional_args_on: if True decorator will also watch through positional arguments
+    :return: inner function with processed Isolated and Evaluated arguments
+    """
     if func is None:
+        if positional_args_on:
+            return lambda func: smart_args(func, positional_args_on=True)
         return lambda func: smart_args(func)
 
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        # print(f"Full inspect: {getfullargspec(func)}")
         func_default_isolated_params_names = []
         func_default_evaluated_params_names = []
         for parameter_name, parameter in signature(func).parameters.items():
             if isinstance(parameter.default, Isolated):
                 func_default_isolated_params_names.append(parameter_name)
-            if (
-                isinstance(parameter.default, Evaluated)
-                and parameter_name not in kwargs.keys()
-            ):
+            if isinstance(parameter.default, Evaluated) and parameter_name not in kwargs.keys():
                 func_default_evaluated_params_names.append(parameter_name)
                 kwargs[parameter_name] = parameter.default.func()
         for kwarg_name, kwarg in kwargs.items():
             if kwarg_name in func_default_isolated_params_names:
                 kwargs[kwarg_name] = deepcopy(kwargs[kwarg_name])
-        # print(f"Function_call args: {args}")
-        if len(args) != 0:
+        if positional_args_on and len(args) != 0:
             args = list(args)
             for i, arg in enumerate(args):
+                if isinstance(arg, Isolated):
+                    args[i] = deepcopy(arg.arg)
                 if isinstance(arg, Evaluated):
                     args[i] = args[i].func()
-            args = tuple(args)
-        result = func(*args, **kwargs)
-        return result
+        return func(*tuple(args), **kwargs)
 
     return inner
-
-
-@smart_args
-def test(*, dict=Isolated()):
-    dict["a"] = 0
-    return dict
-
-
-def get_random_number():
-    return random.randint(0, 100)
-
-
-@smart_args
-def check_evaluation(*, x=get_random_number(), y=Evaluated(get_random_number)):
-    print(x, y)
-
-
-@smart_args
-def check_position(a, b, c=10):
-    a[0] = 1
-    print(f"{a} {b} {c}")
-
-
-@smart_args
-def function(a, b, c=10):
-    print(f"{a} {b} {c}")
-
-
-if __name__ == "__main__":
-    test_dict = {"a": 10}
-    print(test(dict=test_dict))
-    print(test_dict)
-    check_evaluation()
-    check_evaluation()
-    check_evaluation()
-    check_evaluation(y=150)
-    # check_position(Evaluated(get_random_number), b=2)
-    # check_position(Isolated(), b=2)
