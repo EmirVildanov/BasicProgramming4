6d5065bfab56e6481ca153a5ffe5c4e0e6ec2b55
EmirVildanov <reddog201030@gmail.com>
2021-03-04 15:34:28 +0300

    Added tests, typing and docstrings

--- homework/hw2/task2/cash_decorator.py
+++ homework/hw2/task2/cash_decorator.py
@@ -1,56 +1,42 @@
 import functools
 from collections import OrderedDict
-from typing import List, Dict, Tuple, Callable
+from typing import Dict, Tuple, Callable
 
 
-def make_arguments_list(args: Tuple, kwargs: Dict):
-    args = tuple(args)
-    for k, v in kwargs:
+def make_arguments_list(args: Tuple, kwargs: Dict) -> Tuple:
+    """
+    :param args: *args of passing into decorator function
+    :param kwargs: *kwargs of passing into decorator function
+    :return: Tuple of constructed arguments list
+    """
+    args = list(args)
+    for k, v in kwargs.items():
         args.append((k, v))
-    return args
+    return tuple(args)
 
 
-def check_if_in_arguments_list(arguments_list, args: Tuple, kwargs: Dict):
-    for element in args:
-        if element not in arguments_list:
-            return False
-    for k, v in kwargs:
-        if (k, v) not in arguments_list:
-            return False
-    return True
-
-
-def cash(func=None, arguments_cash=OrderedDict(), *, n: int) -> Callable:
+def cash(func=None, *, n: int = 0) -> Callable:
+    """
+    :param func: function to decorate
+    :param n: maximum size of wanted cash
+    :return: inner function
+    """
     if func is None:
         return lambda func: cash(func, n=n)
 
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        if make_arguments_list(args, kwargs) in arguments_cash.keys():
-            print("Returned from cash")
-            return arguments_cash[make_arguments_list(args, kwargs)]
+        if make_arguments_list(args, kwargs) in inner.cash.keys():
+            return inner.cash[make_arguments_list(args, kwargs)]
         arguments = make_arguments_list(args, kwargs)
         result = func(*args, **kwargs)
-        if len(arguments_cash) > n:
-            print(f"Had to delete {arguments_cash.popitem(last=False)}")
-        else:
-            print(f"Len of arg_cash: {len(arguments_cash)}")
-        arguments_cash.update({tuple(arguments): result})
+        inner.function_called_number += 1
+        if len(inner.cash) > n:
+            inner.cash.popitem(last=False)
+        if n != 0:
+            inner.cash.update({tuple(arguments): result})
         return result
 
+    inner.cash = OrderedDict()
+    inner.function_called_number = 0
     return inner
-
-
-@cash(n=3)
-def sum(a: int, b: int):
-    return a + b
-
-
-if __name__ == "__main__":
-    result = sum(1, 2)
-    print(result)
-    result1 = sum(1, 2)
-    print(result1)
-    for i in range(5):
-        result3 = sum(1, i)
-        print(result3)
---
+++ tests/hw2/task2/test_cash.py
@@ -0,0 +1,89 @@
+import unittest.mock
+from typing import Tuple
+
+from homework.hw2.task2.cash_decorator import cash
+
+
+class CashTestCase(unittest.TestCase):
+    def test_should_find_sum_result_with_cash_decorator(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        self.assertEqual(3, custom_sum(1, 2))
+
+    def test_should_cash_result_of_sum_function(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(1, custom_sum.function_called_number)
+
+    def test_should_not_cash_result_of_sum_function(self):
+        @cash()
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(3, custom_sum.function_called_number)
+
+    def test_should_not_cash_result_of_sum_function_with_non_braces_decorator_usage(self):
+        @cash
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(3, custom_sum.function_called_number)
+
+    def test_should_cash_result_of_function_with_args(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int, *args: int):
+            return sum([a, b, *args])
+
+        for i in range(3):
+            custom_sum(1, 2, 3, 4)
+        self.assertEqual(1, custom_sum.function_called_number)
+
+    def test_should_cash_result_of_function_with_kwargs(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int, **kwargs: str):
+            return sum([a, b, *kwargs.keys()])
+
+        for i in range(3):
+            custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(1, custom_sum.function_called_number)
+
+    def test_should_cash_result_of_function_with_both_args_and_kwargs(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        for i in range(3):
+            custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(1, custom_sum.function_called_number)
+
+    def test_should_not_cash_result_of_function_as_passing_different_arguments(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        custom_sum(1, 2, c=4, d=5)
+        custom_sum(1, 1, c=4, d=5)
+        custom_sum(5, 1, c=4, d=5)
+        self.assertEqual(3, custom_sum.function_called_number)
+
+    def test_should_delete_value_from_cash(self):
+        @cash(n=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        custom_sum(1, 2, c=4, d=5)
+        custom_sum(1, 1, c=4, d=5)
+        custom_sum(5, 1, c=4, d=5)
+        custom_sum(5, 1, c=42, d=5)
+        custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(4, custom_sum.function_called_number)
