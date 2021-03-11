8b440abc29a8effa5f8239181c0bdd5f34187ec1
EmirVildanov <reddog201030@gmail.com>
2021-03-11 21:59:34 +0300

    Tried to fix numpy

---
+++ homework/hw2/task2/cache_decorator.py
@@ -0,0 +1,43 @@
+import functools
+from collections import OrderedDict
+from typing import Dict, Tuple, Callable, Any, Hashable
+
+
+def make_arguments_list(args: Tuple, kwargs: Dict) -> Tuple:
+    """
+    :param args: *args of passing into decorator function
+    :param kwargs: *kwargs of passing into decorator function
+    :return: Tuple of constructed arguments list
+    """
+    full_arguments_list = list(args)
+    for k, v in kwargs.items():
+        full_arguments_list.append((k, v))
+    return tuple(full_arguments_list)
+
+
+class FunctionWithCache:
+    def __init__(self, function: Callable = None, cache_size: int = 0):
+        """
+        :param function: function to decorate
+        :param cache_size: maximum size of wanted cache
+        """
+        if function is None:
+            self._function = lambda function: FunctionWithCache(function, cache_size=self._cache_size)
+        else:
+            self._function = function
+        self._cache: OrderedDict = OrderedDict()
+        self._cache_size = cache_size
+        self._function_called_number = 0
+        functools.update_wrapper(self, function)
+
+    def __call__(self, *args: Hashable, **kwargs: Hashable):
+        if make_arguments_list(args, kwargs) in self._cache.keys():
+            return self._cache[make_arguments_list(args, kwargs)]
+        arguments = make_arguments_list(args, kwargs)
+        result = self._function(*args, **kwargs)
+        self._function_called_number += 1
+        if len(self._cache) > self._cache_size:
+            self._cache.popitem(last=False)
+        if self._cache_size != 0:
+            self._cache.update({tuple(arguments): result})
+        return result
---
+++ tests/hw2/task2/test_cache.py
@@ -0,0 +1,88 @@
+import unittest.mock
+
+from homework.hw2.task2.cache_decorator import FunctionWithCache
+
+
+class CacheTestCase(unittest.TestCase):
+    def test_should_find_sum_result_with_cache_decorator(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        self.assertEqual(3, custom_sum(1, 2))
+
+    def test_should_cache_result_of_sum_function(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(1, custom_sum._function_called_number)
+
+    def test_should_not_cache_result_of_sum_function(self):
+        @FunctionWithCache()
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(3, custom_sum._function_called_number)
+
+    def test_should_not_cache_result_of_sum_function_with_non_braces_decorator_usage(self):
+        @FunctionWithCache
+        def custom_sum(a: int, b: int):
+            return a + b
+
+        for i in range(3):
+            custom_sum(1, 2)
+        self.assertEqual(3, custom_sum._function_called_number)
+
+    def test_should_cache_result_of_function_with_args(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int, *args: int):
+            return sum([a, b, *args])
+
+        for i in range(3):
+            custom_sum(1, 2, 3, 4)
+        self.assertEqual(1, custom_sum._function_called_number)
+
+    def test_should_cache_result_of_function_with_kwargs(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int, **kwargs):
+            return sum([a, b, *kwargs.values()])
+
+        for i in range(3):
+            custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(1, custom_sum._function_called_number)
+
+    def test_should_cache_result_of_function_with_both_args_and_kwargs(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        for i in range(3):
+            custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(1, custom_sum._function_called_number)
+
+    def test_should_not_cache_result_of_function_as_passing_different_arguments(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        custom_sum(1, 2, c=4, d=5)
+        custom_sum(1, 1, c=4, d=5)
+        custom_sum(5, 1, c=4, d=5)
+        self.assertEqual(3, custom_sum._function_called_number)
+
+    def test_should_delete_value_from_cache(self):
+        @FunctionWithCache(cache_size=3)
+        def custom_sum(a: int, b: int, *args: int, **kwargs):
+            return sum([a, b, *args, *kwargs.values()])
+
+        custom_sum(1, 2, c=4, d=5)
+        custom_sum(1, 1, c=4, d=5)
+        custom_sum(5, 1, c=4, d=5)
+        custom_sum(5, 1, c=42, d=5)
+        custom_sum(1, 2, c=4, d=5)
+        self.assertEqual(4, custom_sum._function_called_number)
