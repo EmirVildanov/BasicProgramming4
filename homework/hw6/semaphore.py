7958b1003e6125ad841f3b2f5f79a38cee3e69ae
EmirVildanov <reddog201030@gmail.com>
2021-04-21 16:05:59 +0300

    Added custom implementation of Semaphore and wrote tests

--- homework/hw6/semaphore.py
+++ homework/hw6/semaphore.py
@@ -1,10 +1,15 @@
-# Реализовать семафор через менеджер контекстов (доступ к объекту через with).
-# Написать тесты для него используя многопоточность.
+from threading import Lock
+
 
 class Semaphore:
+    """
+    Custom implementation of Semaphore class from threading
+    """
+
     def __init__(self, threads_max_number: int, captured_number: int = 0):
         if captured_number > threads_max_number:
             raise ValueError("[captured_number] parameter can not be bigger than [threads_max_number] parameter")
+        self._lock = Lock()
         self.threads_max_number = threads_max_number
         self.threads_free_number = threads_max_number - captured_number
 
@@ -12,15 +17,28 @@ class Semaphore:
         self.acquire()
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, *args, **kwargs):
         self.release()
 
     def acquire(self):
-        while self.threads_free_number == 0:
-            pass
-        self.threads_free_number -= 1
+        """
+        Function that:
+        passes thread in critical section if self [threads_free_number] field is bigger than 0
+        makes thread wait until self [threads_free_number] field is bigger than 0
+        """
+        while True:
+            with self._lock:
+                if self.threads_free_number > 0:
+                    self.threads_free_number -= 1
+                    break
 
     def release(self):
-        while self.threads_free_number == self.threads_max_number:
-            pass
-        self.threads_free_number += 1
+        """
+        Function that
+        release thread from semaphore
+        or
+        do nothing if self [threads_free_number] field equals self self [threads_max_number] filed
+        """
+        with self._lock:
+            if self.threads_free_number < self.threads_max_number:
+                self.threads_free_number += 1
