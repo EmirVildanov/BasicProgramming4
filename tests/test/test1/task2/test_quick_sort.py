d1b97a7e24e71d44ad2338a1e7f2d7d26320e118
EmirVildanov <reddog201030@gmail.com>
2021-04-23 19:13:21 +0300

    fixed codestyle and added tests

--- tests/test/test1/task2/test_quick_sort.py
+++ tests/test/test1/task2/test_quick_sort.py
@@ -1,21 +1,36 @@
+import random
 import unittest
+from random import shuffle
 
-from homework.tests.test1.task2.quick_sort import get_sorted_numbers_list
-from tests.utils import extract_message_string_from_context
+from homework.tests.test1.task2.quick_sort import quicksort
 
 
 class SmartArgsTestCase(unittest.TestCase):
-    def test_should_raise_error_when_adding_instance_with_already_existed_key(self):
-        with self.assertRaises(ValueError) as context:
-            test = 1
-
-        self.assertTrue(f"Test" in extract_message_string_from_context(context))
-
-    def test_should_check_quicksort_works_correclty(self):
+    def test_should_check_quicksort_works_correctly1(self):
         numbers = [1, 3, 6, 9, 1, 2, 3, 8, 6]
         asserting_numbers = [1, 1, 2, 3, 3, 6, 6, 8, 9]
-        result = get_sorted_numbers_list(numbers)
+        result = quicksort(numbers)
         self.assertEqual(asserting_numbers, result)
+
+    def test_should_check_quicksort_works_correctly2(self):
+        asserting_numbers = [2, 5, 5, 6, 8, 32, 32, 32, 59, 90, 100, 100, 101, 200, 300]
+        numbers = asserting_numbers.copy()
+        shuffle(numbers)
+        result = quicksort(numbers)
+        self.assertEqual(asserting_numbers, result)
+
+    def test_should_check_quicksort_works_correctly_on_big_numbers_list(self):
+        asserting_numbers = list([random.randint(1, 1000) for _ in range(1000)])
+        asserting_numbers.sort()
+        numbers = asserting_numbers.copy()
+        shuffle(numbers)
+        result = quicksort(numbers)
+        self.assertEqual(asserting_numbers, result)
+
+    def test_should_check_quicksort_doing_nothing_on_sorted_list(self):
+        numbers = [2, 5, 5, 6, 8, 32, 32, 32, 59, 90, 100, 100, 101, 200, 300]
+        result = quicksort(numbers)
+        self.assertEqual(numbers, result)
 
 
 if __name__ == "__main__":
